6bab513e2ae251d049f08661dc2084faa777f47d
lhvizza <lucas.vizza@outlook.com>
2023-12-26 08:48:25 -0300

    Create btn and call to modal Beneficiarios

---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/Procedures/FI_SP_AltBenef.sql
@@ -0,0 +1,15 @@
+CREATE PROC [dbo].[FI_SP_AltBenef]
+    @NOME          VARCHAR (50),
+	@CPF           VARCHAR (11),
+	@ID			BIGINT
+AS
+BEGIN
+	UPDATE BENEFICIARIOS 
+	SET 
+		CPF = @CPF,
+		NOME = @NOME
+	WHERE ID = @ID
+END
+GO
+
+
